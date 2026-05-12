83f475da377efb892073c37eb91b7d280824c90a
Alberto Araujo <alberto.araujo.puerta@gmail.com>
2026-05-12 13:05:43 +0200

    fix: CORS Allow-Methods añadir POST (registrar pago Sabadell)

--- ara-os-panel-obras.cjs
+++ ara-os-panel-obras.cjs
@@ -53,7 +53,7 @@ module.exports = function setupAraOSPanelObras(app) {
 
   function responderCORS(res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
 
